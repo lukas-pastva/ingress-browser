3cacdeef736a13a5b282f477a87d766842bf7b4a
Lukas Pastva <lukas.pastva>
2023-09-19 23:07:38 +0200

    feat: initial commit

--- source-code/list_ingress.sh
+++ source-code/list_ingress.sh
@@ -4,13 +4,13 @@ TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 CA_CERT="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 API_SERVER="https://kubernetes.default.svc.cluster.local"
 
-# Fetch ingress resources from all namespaces
+# Fetch Traefik's IngressRoute resources from all namespaces
 if [ -z "$FILTER_PATTERN" ]; then
     curl -s --cacert $CA_CERT -H "Authorization: Bearer $TOKEN" \
-         $API_SERVER/apis/extensions/v1beta1/ingresses | \
-    jq '[.items[] | {name: .metadata.name, namespace: .metadata.namespace, host: .spec.rules[].host}]' > /app/ingresses.json
+         $API_SERVER/apis/traefik.containo.us/v1alpha1/ingressroutes | \
+    jq '[.items[] | {name: .metadata.name, namespace: .metadata.namespace, host: (.spec.routes[].match | match("Host\\(`(.*?)`\\) && PathPrefix\\(`(.*?)`\\)").captures | map(.string) | join(""))}]' > /app/ingresses.json
 else
     curl -s --cacert $CA_CERT -H "Authorization: Bearer $TOKEN" \
-         $API_SERVER/apis/extensions/v1beta1/ingresses | \
-    jq --arg pattern "$FILTER_PATTERN" '[.items[] | select(.metadata.name | test($pattern)) | {name: .metadata.name, namespace: .metadata.namespace, host: .spec.rules[].host}]' > /app/ingresses.json
+         $API_SERVER/apis/traefik.containo.us/v1alpha1/ingressroutes | \
+    jq --arg pattern "$FILTER_PATTERN" '[.items[] | select(.metadata.name | test($pattern)) | {name: .metadata.name, namespace: .metadata.namespace, host: (.spec.routes[].match | match("Host\\(`(.*?)`\\) && PathPrefix\\(`(.*?)`\\)").captures | map(.string) | join(""))}]' > /app/ingresses.json
 fi
