335d3359a1726a2eaa8a027cd3ff615b3a8be2d7
Lukas Pastva <lukas.pastva>
2023-12-18 12:48:52 +0100

    feat: initial commit

--- source-code/list_ingress.sh
+++ source-code/list_ingress.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-CACERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-APISERVER=https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT
+CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+API_SERVER=https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT
 
 # Fetch Nginx Ingress resources from all namespaces
 if [ -z "$FILTER_PATTERN" ]; then
-    curl -s --cacert $CA_CERT -H "Authorization: Bearer $TOKEN" \
-         $API_SERVER/apis/networking.k8s.io/v1/ingresses | \
+    curl -s --cacert $CA_CERT -H "Authorization: Bearer $TOKEN" $API_SERVER/apis/networking.k8s.io/v1/ingresses | \
     jq '[.items[] | {name: .metadata.name, namespace: .metadata.namespace, host: .spec.rules[].host, path: (.spec.rules[].http.paths[]?.path // "/") | gsub("\\(\\.\\*\\)"; "")}]' > /app/ingresses.json
 else
     curl -s --cacert $CA_CERT -H "Authorization: Bearer $TOKEN" \
